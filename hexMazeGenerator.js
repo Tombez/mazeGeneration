b7bab5123a8b8a67d927380b49e6ddff7ebb4f34
Tom Burris <unsaneity@gmail.com>
2017-05-23 07:42:50 -0700

    changed arrays to objects, changed indexing.

--- hexMazeGenerator.js
+++ hexMazeGenerator.js
@@ -1,69 +1,65 @@
 /* User Variables */
-var mazeSize = Math.min(window.innerWidth, window.innerHeight);
-var mazeSpaces = 20; // Number of spaces along the side.
-var startPosition = {x: 0, y: mazeSpaces - 1};
-var finishPosition = {x: mazeSpaces - 1, y: mazeSpaces - 1};
+var gridHeight = Math.min(window.innerWidth, window.innerHeight);
+var r = 9; // Number of spaces between center and corner.
+var startPosition = {x: -r, y: 0};
 /* End User Variables */
 
 Math.ROOT_THREE = Math.sqrt(3);
-var rows = mazeSpaces * 2 - 1;
-var spaceSize = mazeSize / (rows * Math.ROOT_THREE);
-var colOff = (mazeSize - (spaceSize * (1.5 * rows + 0.5))) / 2;
+var cols = r * 2 + 1;
+var hexagonSideLength = gridHeight / (cols * Math.ROOT_THREE);
+var gridWidth = hexagonSideLength * (3/2 * cols + 1/2);
 var canvas;
 var ctx;
 var spaces;
-var solutionPath;
 
 // Basically rainbow tables:
-var directions = [{x: 1, y: -1}, {x: 0, y: -1}, {x: -1, y: 0}, {x: -1, y: 1}, {x: 0, y: 1}, {x: 1, y: 0}]; // quadrant notation.
-var corners = [{x: 2, y: Math.ROOT_THREE/2}, {x: 3/2, y: 0}, {x: 1/2, y: 0}, {x: 0, y: Math.ROOT_THREE/2}, {x: 1/2, y: Math.ROOT_THREE}, {x: 3/2, y: Math.ROOT_THREE}]; // quadrant notation.
+var directions = [{x: 1, y: -1}, {x: 0, y: -1}, {x: -1, y: 0}, {x: -1, y: 1}, {x: 0, y: 1}, {x: 1, y: 0}];
+var directionsLength = directions.length;
+var corners = [{x: 2, y: Math.ROOT_THREE/2}, {x: 3/2, y: 0}, {x: 1/2, y: 0},
+	{x: 0, y: Math.ROOT_THREE/2}, {x: 1/2, y: Math.ROOT_THREE}, {x: 3/2, y: Math.ROOT_THREE}];
 
 function initCanvas() {
 	canvas = document.getElementById("canvas");
 	ctx = canvas.getContext("2d");
 	
-	canvas.width = mazeSize;
-	canvas.height = mazeSize;
+	canvas.width = gridWidth;
+	canvas.height = gridHeight;
 }
 function initSpaces() {
-	spaces = new Array(rows);
-	for (var x = 0; x < rows; x++) {
-		spaces[x] = new Array(rows + Math.min((mazeSpaces - 1) - x, 0));
-		for (var y = Math.max((mazeSpaces - 1) - x, 0); y < spaces[x].length; y++) {
-			spaces[x][y] = new Array(directions.length);
-			for (var n = 0; n <= directions.length; n++) {
-				spaces[x][y][n] = (n != directions.length);
+	spaces = {};
+	for (var x = -r; x <= r; x++) {
+		spaces[x] = {};
+		var start = Math.max(-(r + x), -r);
+		var end = Math.min(r - x, r);
+		for (var y = start; y <= end; y++) {
+			var space = {};
+			spaces[x][y] = space;
+			for (var n = 0; n <= directionsLength; n++) {
+				space[n] = (n != directionsLength);
 			}
 		}	
 	}
 }
 function generate() { // Depth-first backtracker search algorithm from: https://en.wikipedia.org/wiki/Maze_generation_algorithm#Recursive_backtracker.
-	// init generation:
 	var stack = [];
-	var neighbors;
-	var next;
-	var x;
-	var y;
-	var n;
 	var current = {x: startPosition.x, y: startPosition.y};
-
 	while(true) {
-		spaces[current.x][current.y][directions.length] = true;
-		neighbors = [];
-		for (n = 0; n < directions.length; n++) {
-			x = current.x + directions[n].x;
-			y = current.y + directions[n].y;
-			if (spaces[x] && spaces[x][y] && !spaces[x][y][directions.length]) {
+		spaces[current.x][current.y][directionsLength] = true;
+		var neighbors = [];
+		for (var n = 0; n < directionsLength; n++) {
+			var x = current.x + directions[n].x;
+			var y = current.y + directions[n].y;
+			if (spaces[x] && spaces[x][y] && !spaces[x][y][directionsLength]) {
 				neighbors.push(n);
 			}
 		}
 		if (neighbors.length) {
-			next = neighbors[~~(Math.random() * neighbors.length)];
+			var next = neighbors[~~(Math.random() * neighbors.length)];
 			spaces[current.x][current.y][next] = false;
 			stack.push(next);
 			current.x += directions[next].x;
 			current.y += directions[next].y;
-			spaces[current.x][current.y][(next + (directions.length / 2)) % directions.length] = false;
+			spaces[current.x][current.y][(next + (directionsLength / 2)) % directionsLength] = false;
 		} else if (stack.length) { // backtrack:
 			current.x -= directions[stack[stack.length - 1]].x;
 			current.y -= directions[stack[stack.length - 1]].y;
@@ -75,53 +71,50 @@ function generate() { // Depth-first backtracker search algorithm from: https://
 }
 function draw() {
 	ctx.fillStyle = "white"; // Draw background.
-	ctx.fillRect(0, 0, mazeSize, mazeSize);
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	
 	ctx.beginPath(); // Draw walls.
-	ctx.lineWidth = 2;
 	ctx.strokeStyle = "black";
-	var offX = spaceSize * 3 / 2;
-	var offY = spaceSize * Math.ROOT_THREE;
-	for (var x = 0; x < spaces.length; x++) {
-		var start = Math.max((mazeSpaces - 1) - x, 0);
-		var rowOff = (rows - (spaces[x].length - start)) * offY / 2;
-		for (var arrayY = start; arrayY < spaces[x].length; arrayY++) {
-			var y = arrayY - start;
-			for (var n = 0; n < directions.length; n++) {
-				if (spaces[x][arrayY][n]) {
+	ctx.scale(hexagonSideLength, hexagonSideLength);
+	ctx.lineWidth = 2 / hexagonSideLength;
+	for (var x = -r; x <= r; x++) {
+		var drawX = (x + r) * 3/2; // * hexagonSideLength
+		var colTopOffset = (x + 2*r) * Math.ROOT_THREE/2;
+		var yStart = Math.max(-(r + x), -r);
+		var yEnd = Math.min(r - x, r);
+		for (var y = yStart; y <= yEnd; y++) {
+			var drawY = y * Math.ROOT_THREE + colTopOffset; // * hexagonSideLength
+			var space = spaces[x][y];
+			for (var n = 0; n < directionsLength; n++) {
+				ctx.translate(drawX, drawY);
+				if (space[n]) {
 					var nPrime = (n + 1) % corners.length;
-					ctx.moveTo(x * offX + corners[n].x * spaceSize + colOff,
-						y * offY + corners[n].y * spaceSize + rowOff);
-					ctx.lineTo(x * offX + corners[nPrime].x * spaceSize + colOff,
-						y * offY + corners[nPrime].y * spaceSize + rowOff);
+					ctx.moveTo(corners[n].x, corners[n].y);
+					ctx.lineTo(corners[nPrime].x, corners[nPrime].y);
 				}
+				ctx.translate(-drawX, -drawY);
 			}
 		}
 	}
 	ctx.stroke();
 	spaces = null; // release memory.
 }
-function downloadMaze() {
-	var link = document.createElement('a');
-	link.href = canvas.toDataURL();
-	link.download = mazeSpaces + 'WideMaze@' + canvas.width + 'p.png';
-	//document.body.appendChild(link);
-	link.click();
-}
 
 // Main:
 initCanvas();
-initSpaces();
-generate();
+//var startTime = new Date().getTime();
+	initSpaces();
+	generate();
+//console.log("Took: " + (new Date().getTime() - startTime) + "ms");
 requestAnimationFrame(draw);
 
 /* 
- * Useful Hexagon Grid Formulas:
+ * Useful Hexagon/Grid Formulas:
  *
  * hexagon_width = 2 * hexagon_sidelength;
  * hexagon_height = Math.sqrt(3) * hexagon_sidelength;
  *
  * hexagongrid_columns = (hexagongrid_sidenodes * 2) - 1;
- * hexagongrid_height = hexagongrid_columns * hexagon_height;
- * hexagongrid_width = hexagon_sidelength * (1.5 * hexagongrid_columns + 0.5);
+ * hexagongrid_height = Math.sqrt(3) * hexagon_sidelength * hexagongrid_columns;
+ * hexagongrid_width = hexagon_sidelength * (3/2 * hexagongrid_columns + 1/2);
  */
