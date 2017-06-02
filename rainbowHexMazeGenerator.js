73f7574437b775c6d8d7dc4b27cf9856aeee32a4
Tom Burris <unsaneity@gmail.com>
2017-06-02 13:14:50 -0700

    changed background to black

--- rainbowHexMazeGenerator.js
+++ rainbowHexMazeGenerator.js
@@ -4,23 +4,20 @@
 var gridHeight = Math.min(window.innerWidth, window.innerHeight);
 var r = 29; // Number of spaces from center and corner (excluding the center).
 var startPosition = {x: 0, y: 0};
-/* End User Variables */
+var animationTime = 5000; // in ms.
 
+var canvas;
+var ctx;
+var spaces;
 Math.ROOT_THREE = Math.sqrt(3);
 var cols = r * 2 + 1;
 var hexagonSideLength = gridHeight / (cols * Math.ROOT_THREE);
 var gridWidth = hexagonSideLength * (3/2 * cols + 1/2);
-var canvas;
-var ctx;
-var spaces;
-var drawX;
-var drawY;
-var animationTime = 5000; // in ms.
 var nodes = ((r + 1) * r / 2 * 6 + 1);
+var stepTime = animationTime / nodes;
 var colorScaler = ~~(nodes / (1 + nodes / 200));
 
 var directions = [{x: 1, y: -1}, {x: 0, y: -1}, {x: -1, y: 0}, {x: -1, y: 1}, {x: 0, y: 1}, {x: 1, y: 0}];
-var directionsLength = directions.length;
 var corners = [{x: 2, y: Math.ROOT_THREE/2}, {x: 3/2, y: 0}, {x: 1/2, y: 0},
 	{x: 0, y: Math.ROOT_THREE/2}, {x: 1/2, y: Math.ROOT_THREE}, {x: 3/2, y: Math.ROOT_THREE}];
 
@@ -30,25 +27,24 @@ function initCanvas() {
 	canvas.height = gridHeight;
 
 	ctx = canvas.getContext("2d");
-	ctx.fillStyle = "#CCC"; // Draw background.
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	//ctx.fillStyle = "#CCC"; // Draw background.
+	//ctx.fillRect(0, 0, canvas.width, canvas.height);
 	ctx.strokeStyle = "black";
 	ctx.scale(hexagonSideLength, hexagonSideLength);
 	ctx.lineWidth = 2 / hexagonSideLength;
 }
 function initSpaces() {
 	spaces = {};
-	for (var x = -r; x <= r; x++) {
-		spaces[x] = {};
+	for (let x = -r; x <= r; x++) {
+		var spaceX = spaces[x] = {};
 		var start = Math.max(-(r + x), -r);
 		var end = Math.min(r - x, r);
-		for (var y = start; y <= end; y++) {
-			var space = {};
-			spaces[x][y] = space;
-			for (var n = 0; n <= directionsLength; n++) {
-				space[n] = (n != directionsLength);
+		for (let y = start; y <= end; y++) {
+			var space = spaceX[y] = {};
+			for (let n = 0; n < directions.length; n++) {
+				space[n] = true;
 			}
-		}	
+		}
 	}
 }
 var stack = [];
@@ -56,28 +52,35 @@ var current = {x: startPosition.x, y: startPosition.y};
 // Depth-first backtracker search algorithm from: https://en.wikipedia.org/wiki/Maze_generation_algorithm#Recursive_backtracker.
 function generate() {
 	while(true) {
-		spaces[current.x][current.y][directionsLength] = true;
+		var space = spaces[current.x][current.y];
+		space.visited = true;
 		var neighbors = [];
-		for (var n = 0; n < directionsLength; n++) {
-			var x = current.x + directions[n].x;
-			var y = current.y + directions[n].y;
-			if (spaces[x] && spaces[x][y] && !spaces[x][y][directionsLength]) {
-				neighbors.push(n);
+		for (let n = 0; n < directions.length; n++) {
+			var direction = directions[n];
+			var x = current.x + direction.x;
+			var y = current.y + direction.y;
+			var possibleX = spaces[x];
+			if (possibleX) {
+				var possible = possibleX[y];
+				if (possible && !possible.visited) {
+					neighbors.push(n);
+				}
 			}
 		}
 		if (neighbors.length) {
 			var next = neighbors[~~(Math.random() * neighbors.length)];
-			spaces[current.x][current.y][next] = false;
+			space[next] = false;
 			stack.push(next);
-			current.x += directions[next].x;
-			current.y += directions[next].y;
-			spaces[current.x][current.y][(next + (directionsLength / 2)) % directionsLength] = false;
+			var directionNext = directions[next];
+			current.x += directionNext.x;
+			current.y += directionNext.y;
+			spaces[current.x][current.y][(next + (directions.length / 2)) % directions.length] = false;
 		} else { // backtrack:
-			spaces[current.x][current.y].color = stack.length;
+			space.color = stack.length;
 			drawCurrent();
-			current.x -= directions[stack[stack.length - 1]].x;
-			current.y -= directions[stack[stack.length - 1]].y;
-			stack.pop();
+			var directionPrevious = directions[stack.pop()];
+			current.x -= directionPrevious.x;
+			current.y -= directionPrevious.y;
 			break; // breather after drawing.
 		}
 	}
@@ -94,7 +97,7 @@ function fillSpace(x, y, space) {
 	ctx.fill();
 }
 function strokeSpace(x, y, space) {
-	for (var n = 0; n < directionsLength; n++) {
+	for (let n = 0; n < directions.length; n++) {
 		if (space[n]) {
 			var nPrime = (n + 1) % corners.length;
 			ctx.moveTo(corners[n].x, corners[n].y);
@@ -103,35 +106,31 @@ function strokeSpace(x, y, space) {
 	}
 }
 function drawCurrent() {
-	drawX = (current.x + r) * 3/2; // * hexagonSideLength
+	var drawX = (current.x + r) * 3/2; // * hexagonSideLength
 	var colTopOffset = (current.x + 2*r) * Math.ROOT_THREE/2;
-	drawY = current.y * Math.ROOT_THREE + colTopOffset; // * hexagonSideLength
+	var drawY = current.y * Math.ROOT_THREE + colTopOffset; // * hexagonSideLength
 	var space = spaces[current.x][current.y];
 	ctx.translate(drawX, drawY);
 		fillSpace(current.x, current.y, space);
 		ctx.beginPath();
-		strokeSpace(current.x, current.y, space);
+			strokeSpace(current.x, current.y, space);
 		ctx.stroke();
 	ctx.translate(-drawX, -drawY);
 }
 function drawFinal() {
-	drawAll(function(x, y, space) {
-		fillSpace(x, y, space);
-	});
+	drawAll(fillSpace);
 	ctx.beginPath();
-	drawAll(function(x, y, space) {
-		strokeSpace(x, y, space);
-	});
+		drawAll(strokeSpace);
 	ctx.stroke();
 }
 function drawAll(exec) {
-	for (var x = -r; x <= r; x++) {
-		drawX = (x + r) * 3/2; // * hexagonSideLength
+	for (let x = -r; x <= r; x++) {
+		var drawX = (x + r) * 3/2; // * hexagonSideLength
 		var colTopOffset = (x + 2*r) * Math.ROOT_THREE/2;
 		var yStart = Math.max(-(r + x), -r);
 		var yEnd = Math.min(r - x, r);
-		for (var y = yStart; y <= yEnd; y++) {
-			drawY = y * Math.ROOT_THREE + colTopOffset; // * hexagonSideLength
+		for (let y = yStart; y <= yEnd; y++) {
+			var drawY = y * Math.ROOT_THREE + colTopOffset; // * hexagonSideLength
 			ctx.translate(drawX, drawY);
 			exec(x, y, spaces[x][y]);
 			ctx.translate(-drawX, -drawY);
@@ -145,9 +144,8 @@ initSpaces();
 function loop() {
 	if (stack.length) {
 		generate();
-		setTimeout(loop, animationTime / nodes);
+		setTimeout(loop, stepTime);
 	} else {
-		drawCurrent();
 		drawFinal();
 	}
 }
